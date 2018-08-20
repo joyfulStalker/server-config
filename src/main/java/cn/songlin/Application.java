0cbea87390a191e3c0edb5c18a8b2dd3f9666bf5
lsl <liuslc@yonyou.com>
2018-08-20 15:14:31 +0800

    注释

--- src/main/java/cn/songlin/Application.java
+++ src/main/java/cn/songlin/Application.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+/**
+* springboot全局配置
+* @author liusonglin
+* @date 2018年8月20日
+*/
+	
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
